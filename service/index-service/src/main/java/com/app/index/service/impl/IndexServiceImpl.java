0ff2eebdbd02ec46a0d9feadac66fbbb5b8d866f
sunjc <sunjc@startimes.com.cn>
2019-01-09 13:04:36 +0800

    add lombok

--- service/index-service/src/main/java/com/app/index/service/impl/IndexServiceImpl.java
+++ service/index-service/src/main/java/com/app/index/service/impl/IndexServiceImpl.java
@@ -1,14 +1,16 @@
 package com.app.index.service.impl;
 
 import com.app.index.service.IndexService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service("indexService")
 public class IndexServiceImpl implements IndexService {
 
-
     @Override
     public String getIndex() {
+        log.info("IndexServiceImpl getIndex...");
         return "";
     }
 
