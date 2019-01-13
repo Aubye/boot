09de4dbe386e592d1744ecbf31632e3dffb8e17a
sunjc <sunjc@live.cn>
2019-01-13 21:06:18 +0800

    a singletonPatterns

--- learning/design/src/main/java/com/app/design/patterns/singleton/SingletonPatterns.java
+++ learning/design/src/main/java/com/app/design/patterns/singleton/SingletonPatterns.java
@@ -2,13 +2,14 @@ package com.app.design.patterns.singleton;
 
 public class SingletonPatterns {
 
-    private volatile static SingletonObject singletonObject = null;
+    private static volatile SingletonObject singletonObject = null;
 
-    //
     public static SingletonObject getSingletonObject() {
         if (singletonObject != null) {
             synchronized (SingletonPatterns.class) {
-                singletonObject = new SingletonObject();
+                if (singletonObject != null) {
+                    singletonObject = new SingletonObject();
+                }
             }
         }
         return singletonObject;
