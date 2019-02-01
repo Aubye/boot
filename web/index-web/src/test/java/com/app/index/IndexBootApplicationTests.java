068eef55f70f943ee4f8a85f9ca64d5743c2b824
sunjc <sunjc@live.cn>
2019-02-01 21:51:42 +0800

    modify SpringBootTest class

--- web/index-web/src/test/java/com/app/index/IndexBootApplicationTests.java
+++ web/index-web/src/test/java/com/app/index/IndexBootApplicationTests.java
@@ -1,17 +1,14 @@
 package com.app.index;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
 public class IndexBootApplicationTests {
 
     @Test
     public void contextLoads() {
+
     }
 
 }
-
