02ccd07eefd7793d2fea4acf08c8492c024d7ebe
sunjc <sunjc@live.cn>
2019-03-11 21:39:31 +0800

    update dao

--- service/index/index-service/src/main/java/com/app/index/dao/UserRepository.java
+++ service/index/index-service/src/main/java/com/app/index/dao/UserRepository.java
@@ -1,12 +1,12 @@
 package com.app.index.dao;
 
 import com.app.index.domain.po.User;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.scheduling.annotation.Async;
 
 import java.util.stream.Stream;
 
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
     @Async
     Stream<User> findAllByIsDelIsNotNull();
